8abd7bfcc54dc989eb085bc65479b419067db0d5
tha toe saung <thatoe510@gmail.com>
2022-11-17 13:33:32 +0630

    first add

--- src/api/methods.js
+++ src/api/methods.js
@@ -1,4 +1,4 @@
-import { auth } from "../firebase";
+import { auth } from "../config/firebase";
 import { setAxiosConfig } from "./AxiosConfig";
 const axios = require("axios").default;
 //GET METHOD
@@ -6,8 +6,7 @@ export const makeGETAPICall = async (url, additionalHeaders) => {
   try {
     const idToken = await auth.currentUser.getIdToken(true);
     console.log(idToken);
-    console.log(url);
-    if (idToken && axios) {
+    if (idToken) {
       const apiResponse = await axios.get(
         url,
         setAxiosConfig(
@@ -15,7 +14,6 @@ export const makeGETAPICall = async (url, additionalHeaders) => {
           additionalHeaders ? additionalHeaders : undefined
         )
       );
-      console.log(apiResponse, "api response");
       return apiResponse;
     } else {
       throw new Error("Error occurred, please login again to continue");
@@ -23,5 +21,39 @@ export const makeGETAPICall = async (url, additionalHeaders) => {
   } catch (error) {
     console.log("makeGETAPICall newapicall", error, url);
     throw error;
+  }
+};
+
+//PUT METHOD
+
+export const makePUTAPICall = async (url, body) => {
+  try {
+    const idToken = await auth.currentUser.getIdToken(true);
+    if (idToken) {
+      const apiResponse = await axios.put(url, body, setAxiosConfig(idToken));
+      return apiResponse;
+    } else {
+      throw new Error("Error occurred, please login again to continue");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+// POST METHOD
+
+export const makePOSTAPICall = async (url, body) => {
+  try {
+    const idToken = await auth.currentUser.getIdToken(true);
+    var apiResponse = {};
+    if (idToken) {
+      apiResponse = await axios.post(url, body, setAxiosConfig(idToken));
+      return apiResponse;
+    } else {
+      throw new Error("Error occurred, please login again to continue");
+    }
+  } catch (error) {
+    apiResponse.status = 500;
+    return apiResponse;
   }
 };
